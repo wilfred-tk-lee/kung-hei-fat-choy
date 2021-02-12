8a9ab25b6d8e47928f65c89e892449470b6b996d
wilfred-tk-lee <78968590+wilfred-tk-lee@users.noreply.github.com>
2021-02-12 23:31:59 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+basic.forever(function () {
+    basic.showString("Kung Hei Fat Choy")
+    images.createBigImage(`
+        . # . # . # . # . #
+        # # # # # . . . # .
+        . # . # . . # # # #
+        # # # # # . . . # .
+        . # . # . # . # . #
+        `).scrollImage(1, 200)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).scrollImage(3, 100)
+    images.createBigImage(`
+        . # . . . . # . . .
+        . # . # # # . # # #
+        # # # # . # # # . #
+        . # . # # # . # # #
+        . # . . . . # . . .
+        `).scrollImage(1, 200)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).scrollImage(3, 200)
+    images.createBigImage(`
+        # . # . . # # . # #
+        . . # . . # . # . .
+        # # # # . # # . # .
+        . . # . # . . . . #
+        # # . . . # # . . .
+        `).scrollImage(1, 200)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).scrollImage(3, 200)
+    images.createBigImage(`
+        . . # . . . . . . .
+        # # # # # # # # # #
+        . . # . # . . . # .
+        . . # . . # . . . .
+        . . # . . . # . . .
+        `).scrollImage(1, 200)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).scrollImage(3, 200)
+})
